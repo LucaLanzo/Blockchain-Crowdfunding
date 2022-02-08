c5ada4b08dfd9ef399f80846534807d92283a37a
lucalanzo <luca.lanzo21@gmail.com>
2022-02-09 00:10:20 +0100

    added connect wallet button

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {RiMenu3Line, RiCloseLine} from'react-icons/ri';
 import  logo from '../../assets/logo.png'
 import './navbar.css'
+import { init } from '../../web3/Web3Client'
 
 const Menu = () =>(
   <>
@@ -13,6 +14,17 @@ const Menu = () =>(
 
 function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleConnect, setToggleConnect] = useState(true);
+
+  const connectWallet = () => {
+    init().then(tx => {
+      console.log(tx);
+      setToggleConnect(false);
+    }).catch(err => {
+      console.log(err);
+      setToggleConnect(true);
+    })
+  }
 
   return <div className='bcsc__navbar'>
   <div className='bcsc__navbar-links'>
@@ -23,9 +35,11 @@ function Navbar() {
      <Menu />
    </div>
    </div>
-   {/* <div className='bcsc__navbar-connect'>
-     <button type='button'>Connect Wallet</button>
-   </div> */}
+  {toggleConnect&&
+    <div className='bcsc__navbar-connect'>
+      <button type='button' onClick={() => connectWallet()}>Connect Wallet</button>
+    </div>
+  }
    <div className='bcsc__navbar-menu'>
   {toggleMenu
   ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
@@ -35,11 +49,13 @@ function Navbar() {
     <div className='bcsc__navbar-menu_container scale-up-center'>
       <div className='bcsc__navbar-menu_container-links'>
       <Menu />
-      {/* <div className='bcsc__navbar-menu_container-links-connect'>
-     <button type='button'>Connect Wallet</button>
-   </div> */}
+      <div className='bcsc__navbar-menu_container-links-connect'>
+      {toggleConnect&&
+        <button type='button' onClick={() => connectWallet()}>Connect Wallet</button>
+      }
     </div>
-    </div>
+  </div>
+  </div>
   )}
    </div>
   </div>;
