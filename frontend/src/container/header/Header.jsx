05089434d2cbe92cbf2c89b1b75024678069a15b
lucalanzo <luca.lanzo21@gmail.com>
2022-02-09 05:05:26 +0100

    Added some input verification

--- frontend/src/container/header/Header.jsx
+++ frontend/src/container/header/Header.jsx
@@ -11,7 +11,17 @@ function Header() {
   const [deadline, setDeadline] = useState(0);
 
   const createNewProjectMethod = () => {
-    const newProjectEvent = createNewProject(title, descr, goal, deadline);
+    if (goal < 0) {
+      window.alert("Please choose a goal higher than 1!");
+    } else if (deadline < 0) {
+      window.alert("Please choose days higher than 1!");
+    } else if (!(goal > 0 && deadline > 0 && title !== "" && descr !== "")) {
+      window.alert("Please fix your input!");
+    } else {
+      //const newProjectEvent = 
+      createNewProject(title, descr, goal, deadline);
+    }
+   
   }
 
   return <div className='bcsc__header section__padding' id='home'>
@@ -26,13 +36,11 @@ function Header() {
         <div className='input_wrapper'>
         <input type='text' placeholder='Title' onChange={event => setTitle(event.target.value)}/>
         <input type='text' placeholder='Description' onChange={event => setDescr(event.target.value)}/>
-        <input type='number' placeholder='Goal in Wei' onChange={event => setGoal(event.target.value)}/>
-        <input type='number' placeholder='Time in days' onChange={event => setDeadline(event.target.value)} />
+        <input type='number' min="1" placeholder='Goal in Wei' onChange={event => setGoal(event.target.value)}/>
+        <input type='number' min="1" max="365" placeholder='Time in days' onChange={event => setDeadline(event.target.value)} />
         </div>
         <div className='button_wrapper'>
-         <button className='rotate-center' type='button' 
-         onClick={() => createNewProjectMethod()}
-         >
+         <button className='rotate-center' type='button' onClick={() => createNewProjectMethod()}>
          Create Project
          </button>
         </div>
