107dbb6ead85a9ff5ea36d5cccc66b3584546521
lucalanzo <luca.lanzo21@gmail.com>
2022-02-09 05:06:24 +0100

    Typos fixed and editing

--- frontend/src/container/whatFund/WhatFund.jsx
+++ frontend/src/container/whatFund/WhatFund.jsx
@@ -5,17 +5,16 @@ import './whatFund.css'
 function WhatFund() {
   return <div className='bcsc__whatfund section__margin' id='whatfund'>
     <div className='bcsc__whatfund-features'>
-    <Feature title="What is Blockfund" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+    <Feature title="What is Blockfunding?" text="CHANGE" />
   </div>
   <div className='bcsc__whatfund-heading'>
-    <h1 className='gradient__text'>Make things possible in a Crowd</h1>
-    <p>Apes together Strong</p>
+    <h1 className='gradient__text'>The advantages you will get when using Blockfunding</h1>
   </div>
   <div className='bcsc__whatfund-container'>
-  <Feature title="Investments" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Security" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+  <Feature title="Investments" text="CHANGE" />
+      <Feature title="Knowledgebase" text="CHANGE" />
+      <Feature title="Education" text="CHANGE" />
+      <Feature title="Security" text="CHANGE" />
 
   </div>
   </div>;
