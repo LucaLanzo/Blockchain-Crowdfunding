20f29c679acba1c013aa571cdbbba069ed8d1ef6
Adrian_M <33932271+Addi996@users.noreply.github.com>
2022-02-09 11:30:13 +0100

    fixed bug

--- src/container/index.js
+++ src/container/index.js
@@ -1,5 +1,4 @@
 export {default as Header} from './header/Header';
 export {default as Footer} from './footer/Footer';
-export {default as Features} from './features/Features';
 export {default as WhatFund} from './whatFund/WhatFund';
 export {default as ProjectOverview} from './projectOverview/ProjectOverview';
